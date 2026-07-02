de2a4c38eee4f641e5b3f0eb9b8bff67aa5551ca
poisonkumaa <131222732+poisonkumaa@users.noreply.github.com>
2026-07-02 16:39:13 +0900

    Update 順列全列挙.py

--- 順列全列挙.py
+++ 順列全列挙.py
@@ -22,3 +22,16 @@ for i in itertools.combinations_with_replacement(s,3):  #nHr
     s_temp  = "".join(i)
     s3.append(s_temp)
 print(s3)
+
+
+#pythonではmore_itertoolsのdistinct_permutationを用いることで重複なしの並び替えが高速に動作する(len(S) = 10 で700ms)
+#参考：Avoid K Palindrome 2
+from more_itertools import distinct_permutations
+S = "abcdefghij"
+ans = 0
+for s in distinct_permutations(S):
+    #print(s)
+    ans += 1
+print(ans)
+
+
